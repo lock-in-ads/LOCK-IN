a7dfbedd8d24d31ca1583ce2a22f8f66a6859a32
Thiago Araujo <thiagorodrigues12372@gmail.com>
2025-04-08 15:01:04 -0300

    feat: created base model and Address

--- apps/core/models.py
+++ apps/core/models.py
@@ -2,11 +2,55 @@ from django.db import models
 
 # Create your models here.
 class BaseModel(models.Model):
-    criado_em = models.DateTimeField(auto_now_add=True)
-    modificado_em = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
 
-class Endereco(BaseModel):
-    cep = models.PositiveSmallIntegerField()
+class Address(BaseModel):
+    class UF_Choices(models.TextChoices):
+        AC = 'AC', 'Acre'
+        AL = 'AL', 'Alagoas'
+        AP = 'AP', 'Amapá'
+        AM = 'AM', 'Amazonas'
+        BA = 'BA', 'Bahia'
+        CE = 'CE', 'Ceará'
+        DF = 'DF', 'Distrito Federal'
+        ES = 'ES', 'Espírito Santo'
+        GO = 'GO', 'Goiás'
+        MA = 'MA', 'Maranhão'
+        MT = 'MT', 'Mato Grosso'
+        MS = 'MS', 'Mato Grosso do Sul'
+        MG = 'MG', 'Minas Gerais'
+        PA = 'PA', 'Pará'
+        PB = 'PB', 'Paraíba'
+        PR = 'PR', 'Paraná'
+        PE = 'PE', 'Pernambuco'
+        PI = 'PI', 'Piauí'
+        RJ = 'RJ', 'Rio de Janeiro'
+        RN = 'RN', 'Rio Grande do Norte'
+        RS = 'RS', 'Rio Grande do Sul'
+        RO = 'RO', 'Rondônia'
+        RR = 'RR', 'Roraima'
+        SC = 'SC', 'Santa Catarina'
+        SP = 'SP', 'São Paulo'
+        SE = 'SE', 'Sergipe'
+        TO = 'TO', 'Tocantins'
+
+    cep = models.CharField(max_length=8)
+    street = models.CharField(max_length=255, verbose_name="nome_da_rua")
+    number = models.PositiveSmallIntegerField(verbose_name="numero_casa")
+    city = models.CharField(max_length=255)
+    uf = models.CharField(
+        max_length=2,
+        choices=UF_Choices.choices,
+        default=UF_Choices.PB
+    )
+    address_2 = models.CharField(max_length=255, verbose_name="complemento")
+    reference_point = models.CharField(max_length=255, verbose_name="ponto_referencia")
+
+    class Meta:
+        order = ['-created_at']
+        verbose_name = 'Endereco'
+        verbose_name = 'Enderecos'
