5d2d7920dec76c425c8e6f729f580e689efa0673
Paulo Cleidson <167731152+Paulo-Cleidson@users.noreply.github.com>
2025-04-13 15:23:03 -0300

    adicionando tela de cadastrar usuário

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cadastrarusuario(request):
+    return render(request, 'cadastrar-usuario.html')
--- lockin/urls.py
+++ lockin/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.core.views import cadastrarusuario
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('adduser/', cadastrarusuario, name="cadastrar-usuario"),
 ]
