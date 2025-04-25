155867643a1872492bc5d5cf807aba13e46d5505
André Antônio <andreantoniobr@hotmail.com>
2025-04-25 20:23:54 -0300

    feat: added locker assignment form

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.quick_assignment, name='quick-assignment'),
-    path('register-card/', views.register_card, name='register-card'),
-    path('user-list/', views.list_users, name='user-list'),
+    path('', views.quick_assignment, name='quick_assignment'),    
+    path('lockers/', views.list_lockers, name='lockers'),  
+    path('locker/add/', views.add_locker, name='add_locker'),
+    #path('locker/update/<int:id>', views.update_locker, name='update-locker'),
+    #path('locker/delete/<int:id>', views.delete_locker, name='delete-locker'),
+    path('locker/assign/<int:id>', views.assign_locker, name='assign_locker'),
+
+    path('card/add/', views.add_card, name='add_card'),    
+    
+    path('users/', views.users, name='users'),     
+    path('user/add/', views.add_user, name='add_user'), 
 ]
